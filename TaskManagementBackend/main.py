a07abb286646e823924f72ba91f1f5c8de5912ae
Ataullah Masood <info.mdataullah@gmail.com>
2026-03-08 16:26:53 +0530

    created a post endpoint with postgre db

--- TaskManagementBackend/main.py
+++ TaskManagementBackend/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from src.utils.db import Base, Engine
-from src.tasks.models import TaskModel
+#from src.tasks.models import TaskModel  # noqa: F401
+from src.tasks.router import task_routes  #? use f12 to go for its defintion
 
 Base.metadata.create_all(Engine)
 
 app = FastAPI(title="Task Management App")
+
+app.include_router(task_routes)
 
 
 
---
+++ TaskManagementBackend/src/tasks/controller.py
@@ -0,0 +1,26 @@
+from src.tasks.dtos import TaskSchema
+from sqlalchemy.orm import Session
+from src.tasks.models import TaskModel
+
+
+def create_task(body:TaskSchema , db:Session):
+    
+    #print(body.model_dump())
+    data = body.model_dump()
+    
+    new_task = TaskModel(
+        title = data["title"],
+        description = data["description"],
+        is_completed = data["is_completed"]
+    )                        #? TaskModel class ki ek object bani hai jisko humne new_task mein store kr dia hai 
+    
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    
+    return {
+        "status": 200,
+        "msg" : "Task Created Successfully",
+        "data": new_task
+    }
+
